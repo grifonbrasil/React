57f67fb0f0701c99efc64785dcfaad00212462f7
lrdass <lucas.suppes@gmail.com>
2020-08-27 21:52:36 -0300

    Action pra enviar task

--- challenge/src/store/reducers/task.ts
+++ challenge/src/store/reducers/task.ts
@@ -3,10 +3,8 @@ import { TaskState, TaskAction } from "./types";
 const initialState: TaskState[] = [];
 
 const tasks = (state: TaskState[] = initialState, action: TaskAction) => {
-  console.log(action)
   switch (action.type) {
     case 'ADD_TASK':
-      console.log(action.payload)
       return [...state, action.payload]
     default:
       return state
